2ca5147055bc1fda979f5fe67829dd2f3a4935ca
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2025-02-20 02:35:33 +0100

    Se ha añadido el Router necesario para poder usar el Nav junto con un componente Home el cual es la 'pagina' principal del proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { ServicesProvider } from './components/context/ServicesContext';
 import Services from './components/services/Services.js';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './components/home/Home.js';
 
 export default function App() {
     return (
-        <ServicesProvider>
-            <Services/>
-        </ServicesProvider>
+        <BrowserRouter> 
+            <Routes>
+            <Route path="/" element={<Home/>}/>
+
+                <Route path="/payment" element={
+                    <ServicesProvider>
+                        <Services/>
+                    </ServicesProvider>}/>
+
+            </Routes>
+        </BrowserRouter>
     );
 }
